1fe67eb43f551ecbee1d33743eb79a2bb845f92c
codemation <joshjamsion1@gmail.com>
2019-02-05 11:18:37 -0500

    /update_travis.sh3 corrected

--- scripts/update_travis.sh
+++ scripts/update_travis.sh
@@ -1,3 +1,3 @@
-var=$(cat scripts/var)
+var=$(cat scripts/nvar)
 git_branch=$(cat scripts/cur_branch)
 docker image build -f setup/Dockerfile.dev --build-arg build_time_var=$var --build-arg build_time_branch=$git_branch -t joshjamison/ignition setup/
