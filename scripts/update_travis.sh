4f60cae7fe4c5811779e8051c7bac27ab3678fe6
codemation <joshjamsion1@gmail.com>
2019-02-05 08:37:51 -0500

    travis.1

--- scripts/update_travis.sh
+++ scripts/update_travis.sh
@@ -1,3 +1,3 @@
 var=$(cat nvar)
 git_branch=$(cat cur_branch)
-docker image build -f /stuff/py-k8s/setup/Dockerfile.dev --build-arg build_time_var=$var --build-arg build_time_branch=$git_branch -t 
+docker image build -f /py-k8s/setup/Dockerfile.dev --build-arg build_time_var=$var --build-arg build_time_branch=$git_branch -t 
