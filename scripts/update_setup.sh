42b84c138dddb132d6f1638059e6c60d07ef914b
codemation <joshjamsion1@gmail.com>
2019-02-05 10:35:39 -0500

    nf update.10

--- scripts/update_setup.sh
+++ scripts/update_setup.sh
@@ -20,7 +20,7 @@ git commit -m "setup_v0.$var"
 rm -f nvar; rm -f var;
 echo $var >> var
 rm -f cur_branch
-git status | grep "On branch" | awk {'print $3'} >> cur_branch
+git status | grep "On branch" | awk {'print $3'} >> scripts/cur_branch
 git_branch=$(cat cur_branch)
 git push orgin $git_branch 
 echo "current branch is $git_branch"
