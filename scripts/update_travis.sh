120789da2b154d89341ce3cf9d5c9816008899f0
codemation <joshjamsion1@gmail.com>
2019-02-05 10:47:57 -0500

    travis update.5

--- scripts/update_travis.sh
+++ scripts/update_travis.sh
@@ -1,5 +1,3 @@
 var=$(cat nvar)
 git_branch=$(cat cur_branch)
-
-git_branch=$(cat scripts/cur_branch)
 docker image build -f setup/Dockerfile.dev --build-arg build_time_var=$var --build-arg build_time_branch=$git_branch -t joshjamison/ignition setup/
